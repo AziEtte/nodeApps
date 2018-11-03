1570029fe86b44d0fa5a1469833ccbae5c23f8e1
AziEtte <ettehad2@uwm.edu>
2018-11-03 17:28:47 -0500

    Middleware is loaded

--- PracticeMidterm/Server/index.js
+++ PracticeMidterm/Server/index.js
@@ -1,16 +1,22 @@
+var http = require('http');
 var express = require('express');
 
 var app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.use(function(req, res){
+  console.log('Request from ' + req.ip);
+});
 
 app.get('/',function(req,res){
 	res.send('Hello World!');
 });
 
-app.listen(app.get('port'), function(){
-	console.log('Express started on http://localhost:' + app.get('port'));
-});
+http.createServer(app).listen(3000, function(){
+	console.log('Express server listening on port ' + 3000);
+})
+
+
+
 
 
 
