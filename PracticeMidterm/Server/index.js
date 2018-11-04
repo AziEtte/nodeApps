0e880ba2c6374f166866bffee0200b27bb0dfb5f
AziEtte <ettehad2@uwm.edu>
2018-11-04 13:52:33 -0600

    Added new files and codes. Also some changes to the existing codes.

--- PracticeMidterm/Server/index.js
+++ PracticeMidterm/Server/index.js
@@ -1,54 +1,13 @@
-var http = require('http');
 var express = require('express');
-var path = require('path');
-
+var config = require('./config/config');
 
 var app = express();
 
-app.use(express.static(path.normalize(__dirname) + '/public'));
 
-app.use(function(req, res, next){
-  console.log('Request from ' + req.ip);
-  next();
+require('./config/express')(app, config);
+
+require('http').createServer(app).listen(config.port, function () {
+ console.log("HTTP Server listening on port: %d, in %s mode", config.port, app.get('env'));
 });
-
-app.get('/',function(req,res){
-	res.send('Hello World!');
-});
-
-app.get('/about', function(req, res){
-	console.log('about');
-	res.send('About Us!');
-});
-
-app.get('/about/directions', function(req, res){
-	res.send('How to Find Us!');
-});
-
-app.use(function(req, res){
-	res.type('text/plan');
-	res.status(404);
-	res.send('Your page was not found');
-});
-
-app.use(function(err, req, res, next){
-	console.error(err.stack);
-	res.type('text/plan');
-	res.status(500);
-	res.send('500 Sever Error');
-});
-
-
-http.createServer(app).listen(3300, function(){
-    console.log('Express server listening on port ' + 3300);
-})
-
-
-
-
-
-
-
-
 
 
