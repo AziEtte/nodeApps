1c3712eac70f2e36c453ec761265e0f15b1f327c
AziEtte <ettehad2@uwm.edu>
2018-12-10 11:25:14 -0600

    Name of the file chaged

---
+++ PracticeFinalExam/Foo/client/src/modules/foo.js
@@ -0,0 +1,72 @@
+
+import { inject } from 'aurelia-framework';
+import { Router } from 'aurelia-router';
+import { Foo } from '../resources/data/foo-object';
+
+@inject(Router, Foo)
+export class Foo {
+	constructor(router, foo) {
+		this.router = router;
+		this.foo = foo;
+		this.message = 'Foo';
+		this.showFooEditForm = false;
+	}
+
+	async activate() {
+		await this.getFoo();
+	}
+
+	attached() {
+		feather.replace()
+	}
+
+	async getFoo() {
+		await this.foo.getFoo();
+	}
+
+	newFoo() {
+		this.foo = {
+			foo: "",
+			woo: "foo"
+		}
+		this.openEditForm();
+	}
+
+	editFoo(foo) {
+		this.foo = foo;
+		this.showFooEditForm = true;
+	}
+
+	openEditForm() {
+		this.showFooEditForm = true;
+		setTimeout(() => { $("#foo").focus(); }, 500);
+
+	}
+	// changeActive(foo) {
+	// 	this.foo = foo;
+	// 	this.save();
+	// }
+
+	async save() {
+		if (this.foo && this.foo.foo && this.foo.woo)
+			await this.foo.saveFoo(this.foo);
+		await this.getFoo();
+		this.back();
+
+	}
+
+	async delete() {
+		if (this.foo) {
+			await this.foo.delete(this.foo);
+			await this.getFoo();
+			this.back();
+		}
+	}
+
+	back() {
+		this.showFooEditForm = false;
+	}
+
+}
+
+
