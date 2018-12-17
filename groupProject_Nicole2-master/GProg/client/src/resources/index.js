115be835c6ac5bac93beb27746918242ebe9de60
AziEtte <ettehad2@uwm.edu>
2018-12-17 12:24:33 -0600

    Added

--- groupProject_Nicole2-master/GProg/client/src/resources/index.js
+++ groupProject_Nicole2-master/GProg/client/src/resources/index.js
@@ -1,10 +1,9 @@
 export function configure(config) {
   config.globalResources([
-    './elements/nav-bar'
+    './elements/nav-bar',
   ]);
 
   config.globalResources([
     './value-converters/format-date'
   ]);
 }
-  
