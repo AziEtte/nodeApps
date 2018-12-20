5e83def6c5c3bd05bbed5ed4f0cb2a2c95155bd1
AziEtte <ettehad2@uwm.edu>
2018-12-19 18:03:04 -0600

    Authenticated

--- myWebsite/client/src/resources/elements/nav-bar.js
+++ myWebsite/client/src/resources/elements/nav-bar.js
@@ -1,15 +1,24 @@
 import { inject } from 'aurelia-framework';
 import { Router } from 'aurelia-router';
+import { AuthService } from 'aurelia-auth';
 
-@inject(Router)
+
+@inject(Router, AuthService)
 export class NavBar {
-    constructor(router) {
+    constructor(router, auth) {
         this.router = router;
-        this.autenticated = false;
-        this.email = "";
-        this.password = "";
+        this.auth = auth;
+        // this.authenticated = false;
+        this.loginError = '';
+        // this.email = "";
+        // this.password = "";
     }
 
+    bind() {
+        this.isAuthenticated = this.auth.isAuthenticated();
+    }
+    
+    
     attached() {
         $('.navbar-nav a').on('click', function () {
             $('.navbar-nav').find('li.active').removeClass('active');
@@ -18,16 +27,37 @@ export class NavBar {
     }
 
     login() {
-        console.log(this.email);
-        console.log(this.password);
-        this.authenticated = true;
-        this.router.navigate('home');
-    }
+        return this.auth.login(this.email, this.password)
+            .then(response => {
+                this.userObj = response.user;
+                sessionStorage.setItem("userObj", JSON.stringify(this.userObj));
+                this.loginError = "";
+                this.isAuthenticated = this.auth.isAuthenticated();
+                this.router.navigate('home');
+            })
+            .catch(error => {
+                console.log(error);
+                this.authenticated = false;
+                this.loginError = "Invalid credentials.";
+            });
+    };
 
-    logout() {
-        this.authenticated = false;
-        this.router.navigate('landing');
-    }
+    // login() {
+    //     console.log(this.email);
+    //     console.log(this.password);
+    //     this.authenticated = true;
+    //     this.router.navigate('home');
+    // }
 
-
+    // logout() {
+    //     this.authenticated = false;
+    //     this.router.navigate('landing');
+    // }
+    logout(){
+        if (this.userObj) this.auth.logout(this.userObj.email);
+        sessionStorage.removeItem('user');
+        this.isAuthenticated = this.auth.isAuthenticated();
+        this.auth.logout();
+        }
+        
 }
